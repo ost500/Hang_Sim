5da8bee82d631d5cb600149646b08aeb9ce999a8
OST <ost5253@gmail.com>
2016-09-03 18:43:27 +0900

    Update paragraph "hello world"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         $a = Article::where('id','>',1)->get();
-
+        //hello world
         return view('home', compact('a'));
     }
 
